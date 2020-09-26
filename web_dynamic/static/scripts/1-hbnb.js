7466162fe011d6e0b2f5e069ccaa2323d0f6f6d5
danielaloperahernandez <danielaloperahernandez@gmail.com>
2020-09-26 03:11:58 +0000

    AirBnB clone - Web dynamic: Adding tasks 0, 1, 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+$(() => {
+  const amenities = {};
+  $('div.amenities li input').change(
+    () => {
+      if ($(this).is(':checked')) {
+        amenities[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete amenities[($(this).attr('data-id'))];
+      }
+      $('div.amenities h4').text(Object.values(amenities).join(', '));
+    });
+});
